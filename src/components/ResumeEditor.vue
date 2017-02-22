6692fd9c37acaaf4d7e1d85cc0aaf24d51cec411
Hungereeeeee <ycr1714336458@163.com>
2017-02-22 15:21:41 +0800

    采用单向绑定+事件监听

--- src/components/ResumeEditor.vue
+++ src/components/ResumeEditor.vue
@@ -15,15 +15,14 @@
               <div class="subitem" v-for="subitem in resume[item.field]">
                 <div class="resumeField" v-for="(value,key) in subitem">
                   <label>{{key}}</label>
-                  <input type="text" :value="value">
+                  <input type="text" :value="value"  @input="subitem[key] = $event.target.value">
 
                 </div>
               </div>
             </div>
             <div v-else class="resumeField" v-for="(value,key) in resume[item.field]">
               <label>{{key}}</label>
-              <!--<input type="text" :value="value" @input="resume[item.field][key] = $event.target.value">-->
-              <input type="text" v-model="resume[item.field][key]">
+              <input type="text" :value="value" @input="resume[item.field][key] = $event.target.value">
             </div>
         </li>
       </ol>
